1c9648dabfa1e7a53b4828c9e81a2ec6f76b4fe0
Himo / H130 <85074721+Himoxing@users.noreply.github.com>
2026-02-05 12:35:37 +0100

    Model: Add MinecraftServer class with basic properties

---
+++ Minecraft-Server-Maker/Models/MinecraftServer.cs
@@ -0,0 +1,8 @@
+﻿namespace Minecraft_Server_Maker.Models;
+
+public class MinecraftServer
+{
+	public string Name { get; set; } = "My Server";
+	public string JarPath { get; set; } = string.Empty;
+	public int RamMb { get; set; } = 2048;
+}
