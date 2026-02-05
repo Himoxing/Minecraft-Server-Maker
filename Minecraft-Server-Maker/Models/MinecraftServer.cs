8bab3d15c9174a28b8725eb8ff7dd3c8a8842e6d
Himo / H130 <85074721+Himoxing@users.noreply.github.com>
2026-02-05 20:27:28 +0100

    Model: Add two new values - Local Status and Port Number

--- Minecraft-Server-Maker/Models/MinecraftServer.cs
+++ Minecraft-Server-Maker/Models/MinecraftServer.cs
@@ -5,4 +5,6 @@ public class MinecraftServer
 	public string Name { get; set; } = "My Server";
 	public string JarPath { get; set; } = string.Empty;
 	public int RamMb { get; set; } = 2048;
+	public bool Local { get; set; } = false;
+	public int Port { get; set; } = 80;
 }
