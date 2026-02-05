8f83fccc465b6f4a96ade3f6d5560bacc17ace09
Himo / H130 <85074721+Himoxing@users.noreply.github.com>
2026-02-05 13:25:20 +0100

    ViewModel: Add SelectJarCommand and file dialog logic

--- Minecraft-Server-Maker/ViewModels/MainViewModel.cs
+++ Minecraft-Server-Maker/ViewModels/MainViewModel.cs
@@ -1,10 +1,34 @@
-﻿using Minecraft_Server_Maker.Models;
+﻿using System.Windows.Input;
+using Minecraft_Server_Maker.Models;
+using System.Windows.Input;
+using Microsoft.Win32;
 
 namespace Minecraft_Server_Maker.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
 	private MinecraftServer _server = new();
+	
+	public string JarPathDisplay => string.IsNullOrEmpty(_server.JarPath) ? "Not chosen file" : System.IO.Path.GetFileName(_server.JarPath);
+	
+	public ICommand SelectJarCommand { get; }
+
+	public MainViewModel()
+	{
+		SelectJarCommand = new RelayCommand(_ => SelectJarFile());
+	}
+
+	private void SelectJarFile()
+	{
+		OpenFileDialog openFileDialog = new OpenFileDialog();
+		openFileDialog.Filter = "Jar files (*.jar)|*.jar";
+
+		if (openFileDialog.ShowDialog() == true)
+		{
+			_server.JarPath = openFileDialog.FileName;
+			OnPropertyChanged(nameof(JarPathDisplay));
+		}
+	}
 
 	public string ServerName
 	{
