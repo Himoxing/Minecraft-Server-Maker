0d67551c255b7b9f57e78d3059117937f5d45f0b
Himo / H130 <85074721+Himoxing@users.noreply.github.com>
2026-02-06 00:25:13 +0100

    Logic: Use local JRE from Runtime folder and prevent crashes with try-catch

--- Minecraft-Server-Maker/Models/MinecraftServer.cs
+++ Minecraft-Server-Maker/Models/MinecraftServer.cs
@@ -13,27 +13,44 @@ public class MinecraftServer
 
 	public void CreateAndRun()
 	{
-		if (string.IsNullOrEmpty(JarPath)) return;
-
-		string? serverDir = Path.GetDirectoryName(JarPath);
-		if (serverDir == null) return;
-
-		File.WriteAllText(Path.Combine(serverDir, "eula.txt"), "eula=true");
-
-		string serverIp = Local ? "127.0.0.1" : "0.0.0.0";
-		string properties = $"server-port={Port}\n" +
-		                    $"server-ip={serverIp}\n" +
-		                    $"motd={Name}";
-		File.WriteAllText(Path.Combine(serverDir, "server.properties"), properties);
-
-		ProcessStartInfo psi = new ProcessStartInfo
+		try
 		{
-			FileName = "java",
-			Arguments = $"-Xmx{RamMb}M -jar \"{JarPath}\" nogui",
-			WorkingDirectory = serverDir,
-			UseShellExecute = true,
-		};
-		
-		Process.Start(psi);
+			string? serverDir = Path.GetDirectoryName(JarPath);
+
+
+			if (serverDir == null) return;
+
+			string javaPath = Path.Combine(AppContext.BaseDirectory, "Runtime", "bin", "java.exe");
+
+			if (!File.Exists(javaPath))
+			{
+				System.Windows.MessageBox.Show("Jar file doesn't exist!");
+				return;
+			}
+
+
+			File.WriteAllText(Path.Combine(serverDir, "eula.txt"), "eula=true");
+
+			string serverIp = Local ? "127.0.0.1" : "0.0.0.0";
+			string properties = $"server-port={Port}\n" +
+			                    $"server-ip={serverIp}\n" +
+			                    $"motd={Name}";
+			File.WriteAllText(Path.Combine(serverDir, "server.properties"), properties);
+
+			ProcessStartInfo psi = new ProcessStartInfo
+			{
+				FileName = javaPath,
+				Arguments = $"-Xmx{RamMb}M -jar \"{JarPath}\" nogui",
+				WorkingDirectory = serverDir,
+				UseShellExecute = true,
+			};
+
+			Process.Start(psi);
+		}
+
+		catch (Exception ex)
+		{
+			System.Windows.MessageBox.Show(ex.Message);
+		}
 	}
 }
