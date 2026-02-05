678eb3fe007fb4f0d5fefcf9621ebcdf4fd12399
Himo / H130 <85074721+Himoxing@users.noreply.github.com>
2026-02-05 12:48:33 +0100

    ViewModel: Create MainViewModel and expose ServerName

---
+++ Minecraft-Server-Maker/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Minecraft_Server_Maker.Models;
+
+namespace Minecraft_Server_Maker.ViewModels;
+
+public class MainViewModel
+{
+	private MinecraftServer _server = new();
+
+	public string ServerName
+	{
+		get => _server.Name;
+		set
+		{
+			if (_server.Name != value)
+			{
+				_server.Name = value;
+			}
+		}
+	}
+}
